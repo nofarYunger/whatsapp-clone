1369451282282a23b66a08b4642cbacf472d0ca5
Nofar Yunger <nofar505@gmail.com>
2022-02-27 00:06:02 +0200

    finish message design

--- frontend/src/cmps/chat-room/ChatRoomContent.jsx
+++ frontend/src/cmps/chat-room/ChatRoomContent.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Message } from ".";
+
+const msg1 = {
+  content: "hey!! this is my first dummy data try!",
+  date: new Date(),
+  senderId: 2,
+  receiverId: 1,
+  chatId: 3,
+};
+
+const msg2 = {
+  content: "looks great so far! good job to you🥰",
+  date: new Date(),
+  senderId: 1,
+  receiverId: 2,
+  chatId: 3,
+};
 
 function ChatRoomContent() {
   return (
-    <section className='chat-room-content'>
-      
+    <section className="chat-room-content flex col">
+      <Message msg={msg1} />
+      <Message msg={msg2} />
     </section>
-  )
+  );
 }
 
-export default ChatRoomContent
+export default ChatRoomContent;
--- frontend/src/cmps/chat-room/Message.jsx
+++ frontend/src/cmps/chat-room/Message.jsx
@@ -1,9 +1,38 @@
+import { format } from "date-fns";
 import React from "react";
+import { BiCheckDouble, BiCheck } from "react-icons/bi";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
+const user = { title: "nofar", id: 1 };
 
 function Message({ msg }) {
+  const isSender = () => {
+    if (msg.senderId === user.id) return true;
+    return false;
+  };
+
   return (
-    <div className="message-wrapper flex">
-      <div className="message"></div>
+    <div
+      className={`message-wrapper flex ${
+        isSender() ? "message-out" : "message-in"
+      }`}
+    >
+      <div
+        className={`message-container ${
+          isSender() ? "message-out" : "message-in"
+        }`}
+      >
+        <div className="message">
+          <p>{msg.content}</p>
+          <span>
+            <BiCheckDouble />
+          </span>
+          <span>{format(msg.date, "kk:mm")}</span>
+          <div className="options-btn">
+            <MdKeyboardArrowDown />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
